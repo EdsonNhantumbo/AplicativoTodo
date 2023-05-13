0c98d9c6ed85ea5d34962f059812632412d436eb
Edson Nhantumbo <107892569+EdsonNhantumbo@users.noreply.github.com>
2023-05-13 10:31:50 +0100

    Pastas do projecto

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+   <h4 class="q-mt-none">Help</h4>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque esse eveniet tenetur voluptatum distinctio saepe totam, neque rerum explicabo, perferendis vitae obcaecati. Necessitatibus quas inventore optio quidem quod consequatur similique?</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque esse eveniet tenetur voluptatum distinctio saepe totam, neque rerum explicabo, perferendis vitae obcaecati. Necessitatibus quas inventore optio quidem quod consequatur similique?</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
